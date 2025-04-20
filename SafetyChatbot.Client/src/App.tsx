d5e49fd708bcac17e9fb7035fe78b692b4ba3cfd
Sadžida Valjevac <valjevac.sadzida02@gmail.com>
2025-04-20 22:07:22 +0200

    Creating Admin Page

--- SafetyChatbot.Client/src/App.tsx
+++ SafetyChatbot.Client/src/App.tsx
@@ -7,6 +7,7 @@ import { Table, TableHead, TableBody, TableRow, TableCell, Typography, CircularP
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SafetyGuidelinesPage from './pages/SafetyGuidelinesPage';
 import HomePage from './pages/HomePage';
+import AdminPage from './pages/AdminPage';
 
 const App = () => {   
     return (
@@ -15,6 +16,7 @@ const App = () => {
                 <Router>
                     <Routes>
                         <Route path="/" element={<HomePage />} />
+                        <Route path="/admin" element={<AdminPage />} />
                         <Route path="/safety-guidelines" element={<SafetyGuidelinesPage />} />
                         
                     </Routes>
